38421c57a568da178d066a9b3d488c83b399ee84
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-09-24 17:35:42 -0500

    calculadora de descuentos terminada

--- descuentos .js
+++ descuentos .js
@@ -1,1 +1,46 @@
-alert("hola mundo");
+
+
+
+
+function descuentos (precio, descuento){
+    // const precio_con_descuento = (precio_original * (100 - descuento))/ 100;
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+
+function onClickButtonPriceDiscount() {
+    
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = descuentos(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    
+    resultP.innerText = "El precio del producto es de: $ " + priceValue + " " + " El precio con descuento son: $" + precioConDescuento;
+  }
+
+// function calcularPrecioConDescuento(precio, descuento) {
+//     const porcentajePrecioConDescuento = 100 - descuento;
+//     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
+//     return precioConDescuento;
+//   }
+  
+//   function onClickButtonPriceDiscount() {
+//     const inputPrice = document.getElementById("InputPrice");
+//     const priceValue = inputPrice.value;
+    
+//     const inputDiscount = document.getElementById("InputDiscount");
+//     const discountValue = inputDiscount.value;
+  
+//     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  
+//     const resultP = document.getElementById("ResultP");
+//     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+//   }
